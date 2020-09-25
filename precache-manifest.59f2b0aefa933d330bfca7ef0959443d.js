69686a0edcdab6580012e10513e61870328d4140
Michal Michalski <michalm@macbook-air-michal-2.home>
2020-09-25 15:07:39 +0200

    Updates

---
+++ precache-manifest.59f2b0aefa933d330bfca7ef0959443d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092f42fb781be2f89b7fbe1c247b5ab5",
+    "url": "/dotoReact/index.html"
+  },
+  {
+    "revision": "558152f9398f38a464da",
+    "url": "/dotoReact/static/css/main.b4368ef4.chunk.css"
+  },
+  {
+    "revision": "c761029ba0da36151224",
+    "url": "/dotoReact/static/js/2.00987f0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dotoReact/static/js/2.00987f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558152f9398f38a464da",
+    "url": "/dotoReact/static/js/main.d80a258b.chunk.js"
+  },
+  {
+    "revision": "9eb4e0018acaec049369",
+    "url": "/dotoReact/static/js/runtime-main.b1278b26.js"
+  }
+]);
